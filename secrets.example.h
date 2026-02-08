bca438c6ceb49f71eab15c888e10579b3e896e35
benbonn <74539229+benbonn@users.noreply.github.com>
2026-02-08 21:14:05 +0100

    Add WiFi and MQTT configuration to secrets.example.h

---
+++ secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+#define WIFI_SSID     "YOUR_WIFI"
+#define WIFI_PASSWORD "YOUR_PASS"
+#define MQTT_HOST     "192.168.x.x"
+#define MQTT_PORT     1884
+#define MQTT_USER     "user"
+#define MQTT_PASS     "pass"
